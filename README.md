582283edc1631c49b7db8527a227b12ffd63d038
babelfish42 <sandra_beatrice_roth@gmx.ch>
2011-06-10 13:39:50 +0200

    ruby on rails app created

---
+++ README.md
@@ -0,0 +1,7 @@
+Rails Webshop
+
+Have fun with it!
+
+Heroku
+
+The application is deployed at Heroku at: 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babelfish42::Application.config.secret_token = '449eebb158281c78f8714604ca22b4d9760fb5d57d0e07ab1cadd2610def57562bc33ea2a56e0a515f9a0ff2609b8f2a1e1511767e132cc28631be7af8508f86'
